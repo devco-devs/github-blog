fef7613b29fb90cd7f78df0aeddd04e9915f7314
Gabriel Prado <gaprados3@gmail.com>
2022-08-24 21:56:45 -0300

    feat: overwritten styled-components theme type and changed arrow up svg color

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,9 @@
+import 'styled-components'
+import {theme} from '../config/theme';
+
+
+type ThemeType = typeof theme;
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}
