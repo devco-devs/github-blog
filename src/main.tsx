e97b2dac6949f9310caf9b8975af3a7a1f7e637a
Gabriel Prado <gaprados3@gmail.com>
2022-08-24 21:57:27 -0300

    feat: added missing ThemeProvider with theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
 
 import App from './App'
+import { theme } from './config/theme'
 
 import GlobalStyle from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
     <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>
 )
