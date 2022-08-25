b196e3f75ec7632b00b703afe25b8fdcfeebbd59
Gabriel Prado <gaprados3@gmail.com>
2022-08-24 21:57:55 -0300

    feat: added Header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+padding: 64px 0 134px;
+display: flex;
+justify-content: center;
+
+background-image: url('src/assets/cover.png');
+background-repeat: no-repeat;
+background-size: cover;
+`
